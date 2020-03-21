36589e554d0664a1c9ada506b027efe9fd135ac5
dj8yfo <djeaunty8fourmule@xdev.re>
2020-03-21 21:31:51 +0200

    remove expose as heroku dislikes it

--- chat_websockets/run_env.sh
+++ chat_websockets/run_env.sh
@@ -3,6 +3,10 @@
 trap 'kill $(jobs -p)' EXIT INT TERM
 
 #export LOG_DIR=$HOME/hendrix_logs
+sleep 2
+
+python manage.py flush --no-input
+python manage.py migrate
 
 python manage.py msg_process_worker --sub_topic=worker-input --worker_class=websockets_server.core.chat_msg_worker:DbAccessWorker &
 python manage.py msg_process_worker --sub_topic=worker-input-1 --worker_class=websockets_server.core.chat_msg_worker:DbAccessWorker &
